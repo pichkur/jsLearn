92d1963f9a0ca2a1781a5305dd743632162f4caa
Pichkur <pichkur.s@yandex.ru>
2020-02-14 14:46:37 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string' * 5);
+
+let something;
+console.log(something);
+
+let person = {
+  name: 'John',
+  age: 25,
+  isMarried: false
+};
+
+// console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+// console.log(arr[0]);
+
+// alert("hello JS");
+// let answer = confirm("How do you do?");
+
+// console.log(answer);
+
+// let answer = +prompt("Есть ли Вам 18 лет?", "Да");
+
+// console.log(typeof(null));
+
+// console.log(4 + 'ewq');
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5 % 2);
